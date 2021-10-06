c79a86588a827c117fb6f1ec0cd3ec3e856a9cbe
rjrichards27 <89543602+rjrichards27@users.noreply.github.com>
2021-10-06 11:47:28 -0400

    Delete test_hello.py
    Deleting file

--- test_hello.py
+++
@@ -1,9 +0,0 @@
-from click.testing import CliRunner
-from hello import search
-
-
-def test_search():
-    runner = CliRunner()
-    result = runner.invoke(search, ["--path", ".", "--ftype", "py"])
-    assert result.exit_code == 0
-    assert ".py" in result.output
